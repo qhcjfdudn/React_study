144f2183adb3090d22f6118e7b286df9e6a2a8c9
SteelFox <jhjhyjin@naver.com>
2020-04-14 06:41:05 +0900

    [Feat] Add regacy file

---
+++ map_api_test/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      지도지도지도
+    </div>
+  );
+}
+
+export default App;
---
+++ map_api_test/src/map.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MakeMarker() {
+  return (
+    <div className="MakeMarker">
+      
+    </div>
+  );
+}
+
+export default App;
