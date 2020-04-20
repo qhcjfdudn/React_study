d735e9b26f1e7ce2fe05f73e98713e636a29287d
SteelFox <jhjhyjin@naver.com>
2020-04-20 22:20:49 +0900

    [Docs] Modify index

--- toy/src/index.js
+++ toy/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+// import Toggle from './components/Toggle';
+// import WarnToggle from './components/WarnToggle';
 import * as serviceWorker from './serviceWorker';
+import ReduxApp from './components/redux_test/ReduxApp';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ReduxApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
